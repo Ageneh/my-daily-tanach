c300c44a3d816f2f2594e78555d654e462672620
Henock Arega <henock.a@hotmail.de>
2022-12-10 21:22:02 +0100

    Use handler

---
+++ api/current-verse.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+const parseLosungenTanachJson = (year) => {
+	// e.g.: losung-tanach-2022.json
+	const content = fs.readFileSync(`./data/v2/losung-tanach-${year}.json`);
+	const json = JSON.parse(content);
+	console.log("to json ->", json);
+	return json;
+}
+
+const normalizeISODateString = (dateString = undefined) => {
+	if (!dateString || dateString.length < 1) {
+		dateString = new Date().toISOString();
+	}
+	return `${dateString.split("T")[0]}T00:00:00`;
+}
+
+const todayDateString = {
+	"ISO_NORMALIZED": normalizeISODateString(),
+	"SHORT": normalizeISODateString().split("T")[0],
+};
+const getDateData = (dateObject) => {
+	const date = new Date().toISOString();
+	return ({
+		"ISO_NORMALIZED": normalizeISODateString(date),
+		"SHORT": normalizeISODateString(date).split("T")[0],
+	});
+}
+
+const getLosungsDatenV2 = () => {
+	const losungen = parseLosungenTanachJson(new Date().getFullYear());
+	return {
+		date: todayDateString.SHORT,
+		...losungen[todayDateString.SHORT],
+	};
+};
+
+export default function handler(request, response) {
+	response.status(200).json({
+		body: getLosungsDatenV2(),
+	});
+}
